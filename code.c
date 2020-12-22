2ee0d9bdf2c77e17f095536ccca8c8fe2e9c6a0f
SheaneOniridia <fanlapislazuli@gmail.com>
2020-12-22 15:18:55 +0100

    Yolooooo

--- code.c
+++ code.c
@@ -73,11 +73,12 @@ void init_personnage_enne(personnage p){
 
 coup attaque(personnage p, personnage e){ /* p = Attaquant, e = Attaqué*/
   coup coupperso;
-  /*Degats*/
-  coupperso -> degats = (p -> force) * (p->etat->multidmg);
-  /*priorite*/
 
-  coupperso->priorite = (p -> agilite) + (p -> dexterite)*1/2;
+  /*Degats*/
+  coupperso -> degats = (p -> force)*p->etat->multidmg;
+  /*priorite*/
+  coupperso->priorite = (p -> agilite) + (p -> dexterite)*1/4;
+  /*précision de l'action*/
   coupperso -> precision = 90 + p->dexterite - e->agilite
   if ((coupperso ->precision) < 33){
     coupperso -> precision = 33;
@@ -85,12 +86,13 @@ coup attaque(personnage p, personnage e){ /* p = Attaquant, e = Attaqué*/
   /*Etats allie*/
   coupperso->etata->trdef = 0;
   coupperso->etata->esquive =0;
-  coupperso->etata->multidmg = 0;
+  coupperso->etata->multidmg = 1;
   coupperso->etata->traterre = 0;
+
   /*Etats ennemis*/
   coupperso->etate->trdef = 0;
   coupperso->etate->esquive =0;
-  coupperso->etate->multidmg = 0;
+  coupperso->etate->multidmg = 1;
   coupperso->etate->traterre = 0;
 
   return coupperso; /*coup inflige a l'ennemi*/
@@ -115,44 +117,150 @@ coup attaque(personnage p, personnage e){ /* p = Attaquant, e = Attaqué*/
   /*changement d'etat*/
 /*}*/
 
+int gameover(int go){
+  if (go==1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+void victoire(){
+  /*enlever tous les buffs*/
+  /*récuperer des hp a hauteur de 15%*/
+  /*futur mise en place du menu*/
+  printf("victoire!");
+  gameover(1);
+}
+void calcul_attaque_allie(coup a, personnage aa, personnage bb){
+  int precis;
+  precis = random(0,100);
+  if (bb->etat-> esquive == 1){
+    a->precision -= 0.5* bb->agilite
+    if (a->precision <33){
+      a->precision = 33;
+    }
+  }
+  if(a->precision>=precis){
+    bb->vie -= (a->degats*aa->etat->multidmg);
+    /*Etat allie*/
+    aa->etat->trdef += a->etata->trdef;
+    aa->etat->esquive = a->etata->esquive;
+    aa->etat->multidmg = aa->etat->multidmg * a->etata->multidmg;
+    aa->etat->traterre += a->etata->traterre;
+    /*Etats ennemis*/
+    bb->etat->trdef += a->etate->trdef;
+    bb->etat->esquive = a->etate->esquive;
+    bb->etat->multidmg = aa->etate->multidmg * b->etat->multidmg;
+    bb->etat->traterre += a->etate->traterre;
+  }
+  else{
+    if(bb->etat->esquive ==1 && a->degats !=0){
+      bb->etat->esquive = 2;
+    }
+  }
+}
+
+void calcul_attaque_ennemi(coup b, personnage aa, personnage bb){
+  int precis;
+  precis = random(0,100);
+  if (aa->etat-> esquive == 1){
+    b->precision -= 0.5* aa->agilite
+    if (b->precision <33){
+      b->precision = 33;
+    }
+  }
+  if(b->precision>=precis){
+    aa->vie -= (b->degats*bb->etat->multdmg);
+    /*Etats allie*/
+    bb->etat->trdef += b->etata->trdef;
+    bb->etat->esquive = b->etata->esquive;
+    bb->etat->multidmg = bb->etat->multidmg * b->etata->multidmg;
+    bb->etat->traterre += b->etata->traterre;
+    /*Etats ennemis*/
+    aa->etat->trdef += b->etate->trdef;
+    aa->etat->esquive = b->etate->esquive;
+    aa->etat->multidmg = bb->etat->multidmg * b->etate->multidmg;
+    aa->etat->traterre += b->etate->traterre;
+  }
+
+  else{
+    if(bb->etat->esquive ==1 && a->degats !=0){
+       bb->etat->esquive = 2;
+    }
+  }
+}
+
+void findetour(personnage a, personnage b){
+  a->multdmg =1;
+  b->multdmg = 1;
+
+  if(a->etat->trdef > 0){
+    a->etat->trdef -=1;
+    b->etat->multidmg = b->etat->multidmg*0.66;
+  }
+  if(b->etat->trdef > 0){
+    b->etat->trdef -=1;
+    a->etat->mutlidmg = a->etat->multidmg*0.66;
+  }
+
+  if(a->etat->esquive == 2){
+    a->etat->multidmg = a ->etat->multidmg*1.5;
+  }
+  if(b->etat->esquive == 2){
+    b->etat->multidmg = b ->etat->multidmg*1.5;
+  }
+  a->etat->esquive = 0;
+  b->etat->esquive = 0;
+
+  if (a->etat->traterre = 1){
+    b->etat->multidmg = b->etat->multidmg*2;
+    a->etat->traterre = 0;
+  }
+  if (b->etat->traterre = 1){
+    a->etat->multidmg = a->etat->multidmg*2;
+    b->etat->traterre = 0;
+  }
+}
+
 void calcul_du_tour(coup a, coup b, personnage aa, personnage bb){
   int egalite;
   if(a->priorite > b->priorite){
-    aa->etat = a->etata;
-    bb->etat = b->etate;
-    bb->vie -= a->degats;
-    if (bb->vie > 0){
-      aa->vie -= b->degats;
+    calcul_attaque_allie();
+    if (bb->vie >0){
+      calcul_attaque_ennemi();
     }
   }
   if(a->priorite < b->priorite){
-    bb->etat = b->etata;
-    aa->etat = a->etate;
-    aa->vie -= b->degats;
-    if (aa->vie > 0){
-      bb->vie -= a->degats;
+    calcul_attaque_ennemi();
+    if (aa->vie >0){
+      calcul_attaque_allie();
     }
   }
   if(a->priorite == b->priorite){
     egalite = random(0,100);
-    if(egalite >50){
-      aa->etat = a->etata;
-      bb->etat = b->etate;
-      bb->vie -= a->degats;
-      if (bb->vie > 0){
-        aa->vie -= b->degats;
+    if(egalite > 50){
+      calcul_attaque_allie();
+      if (bb->vie >0){
+        calcul_attaque_ennemi();
       }
     }
-    if(egalite <=50){
-      bb->etat = b->etata;
-      aa->etat = a->etate;
-      aa->vie -= b->degats;
-      if (aa->vie > 0){
-        bb->vie -= a->degats;
+    if(egalite <= 50){
+      calcul_attaque_ennemi();
+      if (aa->vie >0){
+        calcul_attaque_allie();
       }
     }
   }
+  if (aa->vie <0){
+    gameover(1);
+  }
+  if (bb->vie <0){
+    victoire();
+  }
+  findetour(aa, bb);
 }
+
 
 
 int main(){
