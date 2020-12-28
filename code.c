a4d734907e8a8dc2d68fca76f08c0eaed77a3053
Hanybay <hanybayazid11@gmail.com>
2020-12-28 20:11:50 +0100

    ligne 321 et 325, deux = au lieu d'un seul, pour faute de comparaison

--- code.c
+++ code.c
@@ -318,11 +318,11 @@ void findetour(personnage a, personnage b){
   a -> etat -> esquive = 0;
   b -> etat -> esquive = 0;
 
-  if (a -> etat -> traterre = 1){
+  if (a -> etat -> traterre == 1){
     b -> etat -> multidmg = b -> etat -> multidmg*2;
     a -> etat -> traterre = 0;
   }
-  if (b->etat->traterre = 1){
+  if (b->etat->traterre == 1){
     a->etat->multidmg = a->etat->multidmg*2;
     b->etat->traterre = 0;
   }
@@ -370,7 +370,7 @@ void explication(){
   printf("Pour jouer à notre fabuleux jeux, vous disposez d'une action parmis 5 chaque tour.\n");
   printf("Attaque : attaque normal, les dégats sont sur la force brut,précision elevée, pas de changement d'état.\n");
   printf("Défense : défense cotre un coup, vous suibissez pour les 2 prochains tours 66 pour cent des dégats normaux, stackable sur 5 tours.\n");
-  printf("Esquive : esquive d'un coup. Reduction des dégats à 100%.\n");
+  printf("Esquive : esquive d'un coup. Reduction des dégats à 100 pour cent\n");
   printf("Labourage : Une chance de mettre a terre l'adversaire au prix d'attaquer en dernier.\n");
   printf("Coup de pied : attaque infligeant de lourds dégats au prix d'une précision moyenne.\n");
 
