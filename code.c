35dd250e542eb523149c91ee42f6c5d6392c2183
Hanybay <hanybayazid11@gmail.com>
2020-12-28 20:05:04 +0100

    % en lettres au lieu du symbole

--- code.c
+++ code.c
@@ -369,7 +369,7 @@ void calcul_du_tour(coup a, coup b, personnage aa, personnage bb){
 void explication(){
   printf("Pour jouer à notre fabuleux jeux, vous disposez d'une action parmis 5 chaque tour.\n");
   printf("Attaque : attaque normal, les dégats sont sur la force brut,précision elevée, pas de changement d'état.\n");
-  printf("Défense : défense cotre un coup, vous suibissez pour les 2 prochains tours 66% des dégats normaux, stackable sur 5 tours.\n");
+  printf("Défense : défense cotre un coup, vous suibissez pour les 2 prochains tours 66 pour cent des dégats normaux, stackable sur 5 tours.\n");
   printf("Esquive : esquive d'un coup. Reduction des dégats à 100%.\n");
   printf("Labourage : Une chance de mettre a terre l'adversaire au prix d'attaquer en dernier.\n");
   printf("Coup de pied : attaque infligeant de lourds dégats au prix d'une précision moyenne.\n");
