075ad645ed125a36bb9a8880645158202e94d807
Hanybay <75792713+Hanybay@users.noreply.github.com>
2020-12-30 21:49:52 +0100

    Add files via upload

---
+++ tour.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "coup.h"
+
+void decisionia(coup a, personnage ia, personnage perso);
+void calcul_attaque(coup a, personnage aa, personnage bb);
+void findetour(personnage a, personnage b);
+void calcul_du_tour(coup a, coup b, personnage aa, personnage bb);
