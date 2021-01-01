ebe620ec202bff4fc2a5c72f02a31d3d94cba2fc
Hanybay <75792713+Hanybay@users.noreply.github.com>
2021-01-01 19:07:51 +0100

    Update save.c

--- save.c
+++ save.c
@@ -7,38 +7,46 @@ void sauvegarde(int *pts,int *nb_ligne,personnage p,personnage e){
   FILE* fichier = NULL;
   fichier = fopen("savegame.txt", "w");
 
+  void sauvegarde(int *pts,int *nb_ligne,personnage p,personnage e){
+
+  FILE* fichier = NULL;
+  int c;
+  fichier = fopen("savetest.txt", "w+");
+
   if(fichier==NULL){
       printf("Erreur lors de l'ouverture du fichier");
       exit(-1);
   }
 
   if (fichier != NULL){
+
     fprintf(fichier,"%s\n",p->nom);
-    fputc(p->force,fichier);
-    fputc(p->agilite,fichier);
-    fputc(p->dexterite,fichier);
-    fputc(p->vitalite,fichier);
-    fputc(p->esquive,fichier);
-    fputc(p->traterre,fichier);
-    fputc(p->multidmg,fichier);
-    fputc(p->trdef,fichier);
-    fputc(*pts,fichier);
-    fputc(*nb_ligne,fichier);
+    fprintf(fichier,"%d\n",p->vie);
+    fprintf(fichier,"%d\n",p->force);
+    fprintf(fichier,"%d\n",p->agilite);
+    fprintf(fichier,"%d\n",p->dexterite);
+    fprintf(fichier,"%d\n",p->vitalite);
+    fprintf(fichier,"%d\n",p->esquive);
+    fprintf(fichier,"%d\n",p->traterre);
+    fprintf(fichier,"%d\n",p->multidmg);
+    fprintf(fichier,"%d\n",p->trdef);
+    fprintf(fichier,"%d\n",*pts);
+    fprintf(fichier,"%d\n",*nb_ligne);
 
 
 
 
     fprintf(fichier,"%s\n",e->nom);
-    fputc(e->force,fichier);
-    fputc(e->agilite,fichier);
-    fputc(e->dexterite,fichier);
-    fputc(e->vitalite,fichier);
-    fputc(e->esquive,fichier);
-    fputc(e->traterre,fichier);
-    fputc(e->multidmg,fichier);
-    fputc(e->trdef,fichier);
-    fputc(*pts,fichier);
-    fputc(*nb_ligne,fichier);
+    fprintf(fichier,"%d\n",e->vie);
+    fprintf(fichier,"%d\n",e->force);
+    fprintf(fichier,"%d\n",e->agilite);
+    fprintf(fichier,"%d\n",e->dexterite);
+    fprintf(fichier,"%d\n",e->vitalite);
+    fprintf(fichier,"%d\n",e->esquive);
+    fprintf(fichier,"%d\n",e->traterre);
+    fprintf(fichier,"%d\n",e->multidmg);
+    fprintf(fichier,"%d\n",e->trdef);
+
     fclose(fichier);
 
   }
