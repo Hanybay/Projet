7969362a2185efdbe30ac50b82ac68efa91003d2
Hanybay <75792713+Hanybay@users.noreply.github.com>
2021-01-02 16:26:25 +0100

    Update save.h
    modif du save.h

--- save.h
+++ save.h
@@ -1,3 +1,4 @@
 #include "tour.h"
 
-void sauvegarde(int *pts,int *nb_ligne,personnage p,personnage e);
+void sauvegarde(int *pts,int *nb_ligne,personnage p,personnage e,int *vic,int *csoigner);
+void load_game(int *pts,int *nb_ligne,personnage p,personnage e,int *vic,int *csoigner);
