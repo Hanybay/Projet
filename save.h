8ac91a863ac910cee61f1eb735c44bc3703c93d9
Hanybay <75792713+Hanybay@users.noreply.github.com>
2021-01-04 02:04:08 +0100

    Update save.h
    update

--- save.h
+++ save.h
@@ -1,5 +1,5 @@
 #include "tour.h"
 
-void save_score(personnage p,int *scoring);
+void save_score(int *scoring);
 void sauvegarde(int *pts,int *nb_ligne,personnage p,personnage e,int *vic,int *csoigner, int *score);
 void load_game(int *pts,int *nb_ligne,personnage p,personnage e,int *vic,int *csoigner, int *score);
